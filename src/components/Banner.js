5ba8a8dc17f0a94180634d0379725bf776c05b37
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2023-04-18 10:51:56 -0600

    Initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,118 @@
+import { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import headerImg from '../assets/img/header-img.svg';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [
+    'Web Developer',
+    'Web Designer',
+    'Frontend Developer',
+    'Fullstack Developer',
+  ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(200);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <section className='banner' id='home'>
+      <Container>
+        <Row className='aligh-items-center'>
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? 'animate__animated animate__fadeIn' : ''
+                  }
+                >
+                  <span className='tagline'>Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Kat a`}{' '}
+                    <span
+                      className='txt-rotate'
+                      dataPeriod='1000'
+                      data-rotate='[ "Web Developer", "Web Designer", "Frontend Developer", "Fullstack Developer"]'
+                    >
+                      <span className='wrap'>{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    As an experienced web developer, I am passionate about
+                    creating clean, modern, and user-friendly websites that meet
+                    my clients' needs. I specialize in building responsive,
+                    mobile-friendly websites. Whether you're a small business
+                    owner looking to establish an online presence, or a larger
+                    organization seeking to revamp your website, I am confident
+                    that I can deliver a customized solution that exceeds your
+                    expectations. Please take a moment to explore my portfolio
+                    and feel free to get in touch if you have any questions or
+                    would like to discuss your project further.
+                  </p>
+                  <button onClick={() => console.log('connect')}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? 'animate__animated animate__zoomIn' : ''
+                  }
+                >
+                  <img src={headerImg} alt='Header Img' />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            {/* <img src={logo} alt="Logo" /> */}
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/katathay"><img src={navIcon1} alt="LinkedIn" /></a>
+              <a href="https://www.github.com/katathay"><img src={navIcon2} alt="github" /></a>
+              <a href="https://www.gmail.com"><img src={navIcon3} alt="Gmail" /></a>
+            <p>Copyright 2022. All Rights Reserved</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
